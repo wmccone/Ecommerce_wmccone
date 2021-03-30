b9f513b3788338d169b3df3735b54759cfcc1418
Walter M <waltermccone88@gmail.com>
2021-03-30 16:58:42 -0500

    added the model for product tag

--- Develop/models/Category.js
+++ Develop/models/Category.js
@@ -14,7 +14,7 @@ Category.init(
       autoIncrement: true,
     },
     category_name: {
-      type: DataTypes.STRING,\
+      type: DataTypes.STRING,
       allowNull: false,
     }
   },
--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -39,7 +39,7 @@ Product.init(
       type: DataTypes.INTEGER,
       references: {
         model: 'category',
-        key: 'id',
+        key: 'category_id',
       },
     },
   },
