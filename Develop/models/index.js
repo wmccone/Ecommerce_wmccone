4eee57eb206bea7e84cd229ea1e8a3d6a0840954
Gerry M <waltermccone88@gmail.com>
2021-03-30 20:14:20 -0500

    created get routes for the category routes

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,25 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category,{
+  foreignKey: 'category_id'
+})
 // Categories have many Products
+Category.hasMany( Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE'
+});
 
 // Products belongToMany Tags (through ProductTag)
+ProductTag.belongsToMany(Tag, {
+  foreignKey: 'tag_id'
+})
 
 // Tags belongToMany Products (through ProductTag)
+Tag.hasMany( ProductTag, {
+  foreignKey: 'tag_id',
+  onDelete: 'CASCADE'
+});
 
 module.exports = {
   Product,
